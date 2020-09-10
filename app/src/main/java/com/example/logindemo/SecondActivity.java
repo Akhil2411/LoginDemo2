f7c224cd5f3c5bedad815b0e48e0be4be5852885
Akhil <akhilraju2411@gmail.com>
2020-09-10 20:46:34 +0530

    Logout in the menubar added

--- app/src/main/java/com/example/logindemo/SecondActivity.java
+++ app/src/main/java/com/example/logindemo/SecondActivity.java
@@ -1,9 +1,12 @@
 package com.example.logindemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,24 +15,52 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private Button logout;
-    private FirebaseAuth firebaseAuth;
+    private Button logout; //button declaration
+    private FirebaseAuth firebaseAuth;  //firebase declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        firebaseAuth=FirebaseAuth.getInstance();
-        logout=(Button)findViewById(R.id.logout1);
+        firebaseAuth=FirebaseAuth.getInstance();   //creating firebase instance
+        logout=(Button)findViewById(R.id.logout1);  //creating button instance
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {   //when button is clicked
             @Override
             public void onClick(View view) {
-                firebaseAuth.signOut();
-                finish();
-                startActivity(new Intent(SecondActivity.this,MainActivity.class));
+                Logout();   //invoke logout function
+
             }
         });
+    }
+        private void Logout(){
+            firebaseAuth.signOut();    //signout from firebase database
+            finish();                  //finish the task
+            startActivity(new Intent(SecondActivity.this,MainActivity.class));  //go to mainactivity
+        }
+
+    //press cntrl+o
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {   //implement the menu in secondactivity
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {   //handle click events on the menu
+        switch (item.getItemId()){
+            case R.id.LogoutMenu:
+                Logout();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
